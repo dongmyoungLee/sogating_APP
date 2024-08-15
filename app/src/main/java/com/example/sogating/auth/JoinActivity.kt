9e328838a6e15bcaa17a0372d7b0197e7f618506
dongmyounglee <pajang1515@daum.net>
2024-08-15 19:06:44 +0900

    Join & Intro Activity Screen adding

--- app/src/main/java/com/example/sogating/auth/JoinActivity.kt
+++ app/src/main/java/com/example/sogating/auth/JoinActivity.kt
@@ -6,18 +6,9 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.sogating.R
 
-class IntroActivity : AppCompatActivity() {
+class JoinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
-
-
-        val joinBtn : Button = findViewById(R.id.joinBtn)
-
-        joinBtn.setOnClickListener {
-            val intent = Intent(this, JoinActivity::class.java)
-            startActivity(intent)
-        }
-
+        setContentView(R.layout.activity_join)
     }
 }
