edc9a8ceeada9d38dff664aa67f0c5705a4b866c
dongmyounglee <pajang1515@daum.net>
2024-08-20 14:28:13 +0900

    Firebase Storage Upload & Download

--- app/src/main/java/com/example/sogating/auth/JoinActivity.kt
+++ app/src/main/java/com/example/sogating/auth/JoinActivity.kt
@@ -1,18 +1,25 @@
 package com.example.sogating.auth
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.example.sogating.MainActivity
 import com.example.sogating.R
 import com.example.sogating.utils.FirebaseRef
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.storage.storage
+import java.io.ByteArrayOutputStream
 
 class JoinActivity : AppCompatActivity() {
 
@@ -26,7 +33,7 @@ class JoinActivity : AppCompatActivity() {
     private var age = ""
     private var uid = ""
 
-
+    lateinit var profileImage : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,8 +42,8 @@ class JoinActivity : AppCompatActivity() {
         // Initialize Firebase Auth
         auth = Firebase.auth
 
+        profileImage = findViewById<ImageView>(R.id.imageArea)
 
-        val profileImage = findViewById<ImageView>(R.id.imageArea)
 
         // 모바일 이미지 불러오기
         val getAction = registerForActivityResult(
@@ -55,6 +62,12 @@ class JoinActivity : AppCompatActivity() {
 
             val email = findViewById<TextInputEditText>(R.id.emailArea)
             val pwd = findViewById<TextInputEditText>(R.id.passwordArea)
+
+            val emailCheck = email.text.toString();
+
+//            if (emailCheck.isEmpty()) {
+//                Toast.makeText(this, "이메일 비었음", Toast.LENGTH_LONG).show()
+//            }
 
             nickname = findViewById<TextInputEditText>(R.id.nicknameArea).text.toString()
             gender = findViewById<TextInputEditText>(R.id.genderArea).text.toString()
@@ -80,17 +93,38 @@ class JoinActivity : AppCompatActivity() {
                         )
 
                         FirebaseRef.userinfoRef.child(uid).setValue(userModel)
+                        uploadImage(uid)
 
-
-//                        val intent = Intent(this, MainActivity::class.java)
-//                        startActivity(intent)
-
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
 
                     } else {
                         // If sign in fails, display a message to the user.
                         Log.w(TAG, "createUserWithEmail:failure", task.exception)
                     }
                 }
+        }
+    }
+
+    private fun uploadImage(uid : String) {
+
+        val storage = Firebase.storage
+        val storageRef = storage.reference.child(uid  + ".png")
+
+        // Get the data from an ImageView as bytes
+        profileImage.isDrawingCacheEnabled = true
+        profileImage.buildDrawingCache()
+        val bitmap = (profileImage.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        var uploadTask = storageRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+        }.addOnSuccessListener { taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
         }
     }
 }
--- app/src/main/java/com/example/sogating/slider/CardStackAdapter.kt
+++ app/src/main/java/com/example/sogating/slider/CardStackAdapter.kt
@@ -1,14 +1,20 @@
 package com.example.sogating.slider
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.sogating.R
 import com.example.sogating.auth.UserDataModel
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.Firebase
+import com.google.firebase.storage.storage
 
 class CardStackAdapter(val context : Context, val items : List<UserDataModel>) : RecyclerView.Adapter<CardStackAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardStackAdapter.ViewHolder {
@@ -29,11 +35,25 @@ class CardStackAdapter(val context : Context, val items : List<UserDataModel>) :
 
     inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
 
+        val image = itemView.findViewById<ImageView>(R.id.profileImageArea)
+
         val nickname = itemView.findViewById<TextView>(R.id.itemNickname)
         val age = itemView.findViewById<TextView>(R.id.itemAge)
         val city = itemView.findViewById<TextView>(R.id.itemCity)
 
         fun binding(data : UserDataModel) {
+
+            val storageRef = Firebase.storage.reference.child(data.uid + ".png")
+
+
+            storageRef.downloadUrl.addOnCompleteListener(OnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Glide.with(context)
+                        .load(task.result)
+                        .into(image)
+                }
+            })
+
             nickname.text = data.nickname
             age.text = data.age
             city.text = data.city
